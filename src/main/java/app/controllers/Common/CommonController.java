f235023ea0076ec0820b4c0bd518c971aaad7a57
vitalygrishin27@gmail.com <vitalygrishinq27>
2020-03-02 20:28:25 +0200

    #77 Statistic bombardiers

--- src/main/java/app/controllers/Common/CommonController.java
+++ src/main/java/app/controllers/Common/CommonController.java
@@ -1,5 +1,6 @@
 package app.controllers.Common;
 
+import app.Models.Context;
 import app.Models.Player;
 import app.Models.User;
 import app.Utils.MessageGenerator;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 @Controller
@@ -23,11 +26,15 @@ public class CommonController {
     @Autowired
     TeamServiceImpl teamService;
 
+    @Autowired
+    Context context;
+
     @GetMapping(value = "/")
     public String getMainPage(Model model) {
         if (messageGenerator.isActive())
             model.addAttribute("message", messageGenerator.getMessageWithSetNotActive());
         model.addAttribute("bombardiers", getGoals(5));
+        model.addAttribute("needShowAllBombardiers", context.getFromContext("needShowAllBombardiers")==null?Boolean.TRUE:context.getFromContext("needShowAllBombardiers"));
         return "administration/mainPage";
     }
 
@@ -51,22 +58,43 @@ public class CommonController {
       }*/
 
     @GetMapping(value = "/bombardiers")
-    public String getAllBombardiers(Model model) {
-        model.addAttribute("bombardiers", getGoals());
+    public String getAllBombardiers(Model model, HttpServletRequest request) {
+        if (request.getParameter("show").equals("all")) {
+            model.addAttribute("bombardiers", getGoals());
+        } else {
+            model.addAttribute("bombardiers", getGoals(5));
+        }
+        model.addAttribute("needShowAllBombardiers", context.getFromContext("needShowAllBombardiers")==null?Boolean.TRUE:context.getFromContext("needShowAllBombardiers"));
         return "common/statistic/bombardiers";
     }
+
     public Map<Player, Integer> getGoals(int count) {
+        if (count == -1 && context.getFromContext("bombardiersAll") != null) {
+            context.putToContext("needShowAllBombardiers", Boolean.FALSE);
+            return (Map<Player, Integer>) context.getFromContext("bombardiersAll");
+        }
+        if (count > 0 && context.getFromContext("bombardiersFirsts") != null) {
+            context.putToContext("needShowAllBombardiers", Boolean.TRUE);
+            return (Map<Player, Integer>) context.getFromContext("bombardiersFirsts");
+        }
+
         Map<Player, Integer> result = new HashMap<>();
         Map<Player, Integer> resultSorted = new LinkedHashMap<>();
         goalService.findAll().forEach(goal -> result.put(goal.getPlayer(), result.containsKey(goal.getPlayer()) ? result.get(goal.getPlayer()) + 1 : 1));
         // TODO: 02.03.2020 create method or constant of AUTOGOAL instead below code
         result.remove(teamService.findTeamByName("AUTOGOAL").getPlayers().toArray()[0]);
         result.entrySet().stream().sorted(Map.Entry.<Player, Integer>comparingByValue().reversed()).forEach(e -> resultSorted.put(e.getKey(), e.getValue()));
+        Map<Player, Integer> resultSortedFirsts = resultSorted.entrySet().stream().limit(count).collect(LinkedHashMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
+        context.putToContext("bombardiersAll", resultSorted);
+        context.putToContext("bombardiersFirsts", resultSortedFirsts);
+
         if (count > resultSorted.size()) count = -1;
         if (count == -1) {
+            context.putToContext("needShowAllBombardiers", Boolean.FALSE);
             return resultSorted;
         }
-        return resultSorted.entrySet().stream().limit(count).collect(LinkedHashMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
+        context.putToContext("needShowAllBombardiers", Boolean.TRUE);
+        return resultSortedFirsts;
     }
 
     public Map<Player, Integer> getGoals() {
