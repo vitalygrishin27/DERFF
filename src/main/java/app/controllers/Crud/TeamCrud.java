2f04d445a9bed9cc3b3b6e406937174bff7fe1ac
Gryshyn Vitaliy <vitaliy.gryshyn@extern.a1.at>
2020-05-22 21:56:10 +0300

    Cross origin

--- src/main/java/app/controllers/Crud/TeamCrud.java
+++ src/main/java/app/controllers/Crud/TeamCrud.java
@@ -5,6 +5,7 @@ import app.services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import java.util.Collection;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class TeamCrud {
     @Autowired
     TeamService teamService;
