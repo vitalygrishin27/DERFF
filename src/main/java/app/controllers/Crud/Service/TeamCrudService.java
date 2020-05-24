6b2d9bb46b4442d969369b955a029ec6f0226401
Gryshyn Vitaliy <vitaliy.gryshyn@extern.a1.at>
2020-05-24 16:40:29 +0300

    post method

---
+++ src/main/java/app/controllers/Crud/Service/TeamCrudService.java
@@ -0,0 +1,72 @@
+package app.controllers.Crud.Service;
+
+import app.Models.Team;
+import app.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+
+@Service
+public class TeamCrudService {
+
+    @Value("${maxUploadFileSizeTeamSymbol}")
+    private Long maxUploadFileSizeTeamSymbol;
+
+    @Value("${maxUploadFileSizePlayerPhoto}")
+    private Long maxUploadFileSizePlayerPhoto;
+
+    @Value("${availableFileExtension}")
+    private String availableFileExtension;
+
+    @Autowired
+    TeamService teamService;
+
+    public HttpStatus saveTeamFlow(Team team, MultipartFile file) {
+        //validate Team name
+        if (teamService.findTeamByName(team.getTeamName()) != null && team.getId() == 0) {
+            return HttpStatus.PRECONDITION_FAILED;
+        }
+        // File size validation
+        if (file != null && file.getSize() > maxUploadFileSizeTeamSymbol) {
+            return HttpStatus.PRECONDITION_FAILED;
+        }
+        // File extension validation
+        if (file != null && file.getSize() > 0) {
+            boolean isCorrectFileExtention = false;
+            for (String regex : availableFileExtension.split(";")
+            ) {
+                if (file.getOriginalFilename().endsWith(regex)) {
+                    isCorrectFileExtention = true;
+                    break;
+                }
+            }
+            if (!isCorrectFileExtention) {
+                return HttpStatus.PRECONDITION_FAILED;
+            }
+            //Set byte[] to Team
+            try {
+                if (team.getSymbolString() == null) {
+                    byte[] bytes = file.getBytes();
+                    team.setSymbolString("data:image/jpeg;base64, " + Base64Utils.encodeToString(bytes));
+                    team.setSymbol(bytes);
+                } else if (team.getId() != 0) {
+                    team.setSymbol(teamService.findTeamById(team.getId()).getSymbol());
+                    team.setSymbolString(teamService.findTeamById(team.getId()).getSymbolString());
+                }
+            } catch (IOException e) {
+                return HttpStatus.PRECONDITION_FAILED;
+            }
+        }
+        try {
+            teamService.save(team);
+            return HttpStatus.OK;
+        } catch (Exception e) {
+            return HttpStatus.PRECONDITION_FAILED;
+        }
+    }
+
+}
--- src/main/java/app/controllers/Crud/TeamCrud.java
+++ src/main/java/app/controllers/Crud/TeamCrud.java
@@ -2,43 +2,28 @@ package app.controllers.Crud;
 
 import app.Models.Team;
 import app.Utils.MessageGenerator;
-import app.controllers.Administration.TeamController;
-import app.exceptions.DerffException;
+import app.controllers.Crud.Service.TeamCrudService;
 import app.services.TeamService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 
 @RestController
 @CrossOrigin
 public class TeamCrud {
-    @Value("${maxUploadFileSizeTeamSymbol}")
-    private Long maxUploadFileSizeTeamSymbol;
-
-    @Value("${maxUploadFileSizePlayerPhoto}")
-    private Long maxUploadFileSizePlayerPhoto;
-
-    @Value("${availableFileExtension}")
-    private String availableFileExtension;
-
-    @Autowired
-    MessageGenerator messageGenerator;
-
-    @Autowired
-    ReloadableResourceBundleMessageSource messageSource;
 
     @Autowired
     TeamService teamService;
+
+    @Autowired
+    TeamCrudService teamCrudService;
 
     @RequestMapping("/ui/teams")
     public ResponseEntity<Collection<Team>> getAllTeamNames() {
@@ -50,61 +35,18 @@ public class TeamCrud {
     }
 
     @PostMapping("/ui/team")
-    public ResponseEntity<String> saveNewTeam(@ModelAttribute Team team, @RequestParam(value = "file", required = false) MultipartFile file) throws DerffException {
-       validateTeamInformation(team, file, true);
-        try {
-            teamService.save(team);
-            messageGenerator.setMessage((messageSource
-                    .getMessage("success.newTeam", new Object[]{team.getTeamName()}, Locale.getDefault())));
-        } catch (Exception e) {
-            throw new DerffException("database", team, new Object[]{e.getMessage()});
-        }
-        return new ResponseEntity<>(String.valueOf(teamService.findTeamByName(team.getTeamName()).getId()), HttpStatus.OK);
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Team saved successfully"),
+            @ApiResponse(code = 412, message = "Precondition Failed"),
+           // @ApiResponse(code = 501, message = "SLA's not found"),
+          //  @ApiResponse(code = 403, message = "SLA's update not possible"),
+         //   @ApiResponse(code = 406, message = "Incorrect SLA's Times definition"),
+    })
+    public ResponseEntity saveNewTeam(@ModelAttribute Team team, @RequestParam(value = "file", required = false) MultipartFile file)  {
+        return ResponseEntity.status(teamCrudService.saveTeamFlow(team,file)).build();
     }
 
 
-    private void validateTeamInformation(Team team, MultipartFile file, boolean needToReplaceFile) throws DerffException {
-        //validate Team name
-        if (teamService.findTeamByName(team.getTeamName()) != null && team.getId() == 0) {
-            throw new DerffException("notAvailableTeamName", team);
-        }
 
-        // File size validation
-        if (file != null && file.getSize() > maxUploadFileSizeTeamSymbol)
-            throw new DerffException("maxUploadFileSizeTeamSymbol", team, new Object[]{maxUploadFileSizeTeamSymbol, file.getSize()});
-
-        // File extension validation
-        if (file != null && file.getSize() > 0) {
-            boolean isCorrectFileExtention = false;
-            for (String regex : availableFileExtension.split(";")
-            ) {
-                if (file.getOriginalFilename().endsWith(regex)) {
-                    isCorrectFileExtention = true;
-                    break;
-                }
-            }
-            if (!isCorrectFileExtention)
-                throw new DerffException("notAvailableFileExtension", team, new Object[]{availableFileExtension});
-
-            //Set byte[] to Team
-            try {
-                if (team.getSymbolString() == null || needToReplaceFile) {
-                    byte[] bytes = file.getBytes();
-                    team.setSymbolString("data:image/jpeg;base64, " + Base64Utils.encodeToString(bytes));
-                    team.setSymbol(bytes);
-                } else if (team.getId() != 0) {
-                    team.setSymbol(teamService.findTeamById(team.getId()).getSymbol());
-                    team.setSymbolString(teamService.findTeamById(team.getId()).getSymbolString());
-                }
-            } catch (IOException e) {
-                throw new DerffException("fileGetBytes", team, new Object[]{e.getMessage()});
-            }
-        } else {
-            if (!needToReplaceFile && team.getId() != 0) {
-                team.setSymbol(teamService.findTeamById(team.getId()).getSymbol());
-                team.setSymbolString(teamService.findTeamById(team.getId()).getSymbolString());
-            }
-        }
-    }
 
 }
