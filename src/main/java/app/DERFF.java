e3a5a875d275cdc96c3c7d02af3e4dca6ee6713a
Gryshyn Vitaliy <vitaliy.gryshyn@extern.a1.at>
2020-07-12 14:53:14 +0300

    PlayersForStatistic add id

--- src/main/java/app/DERFF.java
+++ src/main/java/app/DERFF.java
@@ -8,12 +8,6 @@ import java.util.Locale;
 
 @SpringBootApplication
 public class DERFF {
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        return factory.createMultipartConfig();
-    }
-
     public static void main(String[] args) {
    //     ApplicationContext context=new AnnotationConfigApplicationContext(Context.class);
      //   https://stackoverflow.com/questions/32650536/using-thymeleaf-variable-in-onclick-attribute
